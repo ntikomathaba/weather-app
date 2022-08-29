4220946fa64a3ce921fe63a134a848e2010dd755
NtikoM <NtikoM@astrocyte.healthcare>
2022-08-29 20:31:43 +0200

    Removed map viewmodel from the Mapscreen

--- app/src/main/java/com/weather/compose/screen/MapScreen.kt
+++ app/src/main/java/com/weather/compose/screen/MapScreen.kt
@@ -4,12 +4,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.weather.compose.components.WeatherBottomAppBar
-import com.weather.viewmodel.MapsViewModel
 
 @Composable
 fun MapScreen(
     navController: NavController,
-    viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
     WeatherBottomAppBar(
         navController = navController,
